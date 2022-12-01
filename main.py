ea9b0528a4444c6820be6fb86735ba9a5410d359
Estis <kulaykin2000@gmail.com>
2022-12-01 20:03:38 +0600

    it works

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import info
 import maps
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pyautogui.PAUSE = 0.1
 
 # Set up canstants
 PATH = 'images/'
@@ -44,12 +45,11 @@ def press_key(key, count=1, duration=0, wait_time_after_press=DEFAULT_WAIT_TIME)
     else: key = list(key)
     for i in range(count):
         for k in key: pyautogui.keyDown(k)
-        event_listener(duration)
+        time.sleep(duration)
         for k in key: pyautogui.keyUp(k)
     event_listener(wait_time_after_press)
 
 def click(xy, button='left', clicks=1, wait_time_after_click=DEFAULT_WAIT_TIME, click_duration=0.1):
-    print(f'Clicking {xy}', time.strftime('%H:%M:%S'))
     pyautogui.moveTo(*xy)
     pyautogui.click(button=button, clicks=clicks, duration=click_duration)
     event_listener(wait_time_after_click)
@@ -63,7 +63,7 @@ def find_image(image_name,
     while True:
         localtion = pyautogui.locateOnScreen(image, grayscale=grayscale, confidence=confidence)
         if localtion:
-            print(f'Found {image_name}', time.monotonic())
+            print(f'Found {image_name}', time.strftime('%X', time.localtime()))
             if click:
                 pyautogui.moveTo(localtion)
                 pyautogui.click(button=button, clicks=clicks, duration=click_duration, interval=interval)
@@ -106,12 +106,46 @@ def get_coin_value(coin_image_name):
     # convert to gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # read the value
-    value = pytesseract.image_to_string(gray, config='--psm 6')
+    value = pytesseract.image_to_string(gray, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789 ,')
+    value = value.replace(',', '')
+    value = value.replace(' ', '')
     try:
         value = int(value)
     except:
         value = 0
     return value
+
+def get_upgrade_cost(left=1859, top=908, width=61, height=22):
+    region = {'top': top, 'left': left, 'width': width, 'height': height}
+    # screen capture
+    with mss.mss() as sct:
+        img = np.array(sct.grab(region))
+    # convert to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # read the value
+    value = pytesseract.image_to_string(gray, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789 ,')
+    value = value.replace(',', '')
+    value = value.replace(' ', '')
+    print('Upgrade cost: ', value)
+    # Show the image
+    # cv2.imshow('image', gray)
+    # print(value)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # exit()
+    try:
+        value = int(value)
+    except:
+        value = 1000000000
+    return value
+
+def update_offset(rtb, _rewards, _offset, value):
+    if value < 0: value = 0
+    if len(rtb) == 0:
+        _offset = info.mods['reward']['max'] / (info.mods['reward']['percent'] / 100) if _rewards > 0 else 0
+    else:
+        _offset = _offset+value/2
+    return _offset
 
 def farm_1(this_map=maps.farm_1.copy()):
     find_image('level_redactor')
@@ -121,6 +155,8 @@ def farm_1(this_map=maps.farm_1.copy()):
         find_image('play')
     # Press ] to speed up twice
     press_key(']', 2)
+    pyautogui.moveTo(0, 300)
+    pyautogui.dragTo(0, 350, 0.25)
     grid = get_game_map_grid(this_map['cell_1'], this_map['cell_2'], this_map['width'], this_map['height'])
     # Get towers for this map
     towers_to_build = list(this_map['towers'].keys())
@@ -129,30 +165,60 @@ def farm_1(this_map=maps.farm_1.copy()):
     for i in range(len(towers_to_build)):
         tower = towers_to_build[i]
         towers_info[tower] = {}
-        towers_info[tower]['cels'] = []
-        for j in range(len(this_map['towers'][tower]['cels'])):
-            x, y = this_map['towers'][tower]['cels'][j]
-            towers_info[tower]['cels'].append((x, y))
+        towers_info[tower]['cells'] = []
+        for j in range(len(this_map['towers'][tower]['cells'])):
+            x, y = this_map['towers'][tower]['cells'][j]
+            towers_info[tower]['cells'].append((x, y))
 
-    # Get the location of the first tower
-    tower_location = towers_info[towers_to_build[0]]['cels'][0]
-    # Build the first tower
-    click(grid[tower_location])
-    press_key(info.towers[towers_to_build[0]]['hotkey'])
-    # Remember location of the first tower of this type
-    towers = {towers_to_build[0]: tower_location}
-    # Remove this position from the list
-    towers_info[towers_to_build[0]]['cels'].pop(0)
+    towers_order = []
+    for i in range(max([len(towers_info[tower]['cells']) for tower in towers_info])):
+        for tower in towers_info:
+            if len(towers_info[tower]['cells']) % (i+1) == 0:
+                towers_order.append(tower)
+    towers = {}
+    mods = {}
 
-    # Get the location of the second tower
-    tower_location = towers_info[towers_to_build[1]]['cels'][0]
-    # Build the second tower
-    click(grid[tower_location])
-    press_key(info.towers[towers_to_build[1]]['hotkey'])
-    # Remember the location of the first tower of this type
-    towers[towers_to_build[1]] = tower_location
-    # Remove this position from the list
-    towers_info[towers_to_build[1]]['cels'].pop(0)
+    mods_to_build = []
+    rewards_to_build = []
+    for mod in list(this_map['mods'].keys()):
+        for i, cell in enumerate(this_map['mods'][mod]['cells']):
+            if mod != 'reward':
+                mods_to_build.append({
+                    'mod': mod,
+                    'cell': cell,
+                    'cost': info.mods[mod]['costs'][i],
+                })
+            else:
+                rewards_to_build.append({
+                    'mod': mod,
+                    'cell': cell,
+                    'cost': info.mods[mod]['costs'][i],
+                })
+    # Sort mods by cost
+    mods_to_build.sort(key=lambda x: x['cost'])
+    rewards = 0 # This mod gets info.mods['reward']['percent']% from existing coins as a reward per wave, but no more than info.mods['reward']['max']
+    offset = 0
+    first_towers = {}
+    tasks = {}
+
+    for tower in towers_info:
+        for i, cell in enumerate(towers_info[tower]['cells']):
+            tasks[cell] = {
+                'name': tower,
+                'type': 'build',
+                'object_type': 'tower',
+                'object': tower,
+                'cost': info.towers[tower]['cost'],
+            }
+    for mod in mods_to_build:
+        tasks[mod['cell']] = {
+            'name': mod['mod'],
+            'type': 'build',
+            'object_type': 'mod',
+            'object': mod['mod'],
+            'cost': mod['cost'],
+        }
+
 
     core_upgrades = ['uc1', 'uc2', 'uc3', 'uc4', 'uc5', 'uc6', 'uc7', 'uc8', 'uc9', 'uc10', 'uc11', 'uc12', 'uc13', 'uc14', 'uc15', 'uc16']
 
@@ -161,49 +227,126 @@ def farm_1(this_map=maps.farm_1.copy()):
     # Loop until the game is over
     while True:
         # Are we in the game?
-        # if not find_image('game_on_taskbar', wait_duration=3, if_not_found='nothing'): ...
+        # if not find_image('game_on_taskbar', wait_duration=3, if_not_found='nothing'): ... TODO
         # If the game is over
         if find_image('menu', wait_duration=0, if_not_found='nothing'):
             print('Game over at', time.strftime('-----------------%X', time.localtime()))
+            event_listener(1)
+            find_image('menu', wait_duration=0, if_not_found='nothing')
             press_key('esc')
-            event_listener(5)
+            event_listener(1)
             return farm_1()
         
-        # If there are still towers to build
-        # And if we have enough coins to build the next tower
-        while len(towers_to_build) > 0 and get_coin_value('coin') >= info.towers[towers_to_build[0]]['cost']:
-            # Get the location of the next tower
-            tower_location = towers_info[towers_to_build[0]]['cels'][0]
-            # Build the next tower
-            click(grid[tower_location])
-            press_key(info.towers[towers_to_build[0]]['hotkey'])
-            # Remember the location of the first tower of this type (if there is the first tower of this type)
-            if towers_to_build[0] not in towers: towers[towers_to_build[0]] = tower_location
-            # Remove this position from the list
-            towers_info[towers_to_build[0]]['cels'].pop(0)
-            # Remove this tower from the list if there are no more positions
-            if len(towers_info[towers_to_build[0]]['cels']) == 0: towers_to_build.pop(0)
-        
         # Check if the core(is the cell 1) needs to be upgraded
-        click(grid[this_map['cell_1']['position']])
-        for upgrade in core_upgrades:
-            location = find_image(upgrade, grayscale=False, if_not_found='nothing', wait_duration=0, confidence=0.95)
-            if location: click((location.left, location.top))
-
-        # Upgrade all the towers
-        for tower in towers:
-            click(grid[towers[tower]])
-            # Press cntrl+shift to upgrade all the towers of same type by 1 level if there are enough coins
-            press_key(['ctrl', 'shift'])
-            # Press cntrl+num upgredes to select the upgrade to be applied to all the towers of same type if there are enough xp level of the tower
-            for upgrade in info.towers[tower]['num upgrades']:
-                press_key(['ctrl', upgrade])
+        if len(core_upgrades) > 0:
+            click(grid[this_map['cell_1']['position']])
+            location = find_image(core_upgrades[0], grayscale=False, if_not_found='nothing', wait_duration=0, confidence=0.95)
+            if location:
+                click((location.left, location.top))
+                core_upgrades.pop(0)
         
+        coins = get_coin_value('coin')
+        available_coins = coins - offset
+            
+        # If there are still rewards to build
+        if len(rewards_to_build) > 0 and coins > rewards_to_build[0]['cost']:
+            # Calculate the current reward
+            current_reward = min(offset * info.mods['reward']['percent'] / 100, info.mods['reward']['max']) * rewards
+            # Calculate the new reward
+            new_reward = min((coins-rewards_to_build[0]['cost']) * info.mods['reward']['percent'] / 100, info.mods['reward']['max']) * (rewards+1)
+            if new_reward > current_reward:
+                # Build the next reward
+                click(grid[rewards_to_build[0]['cell']])
+                press_key(info.mods[rewards_to_build[0]['mod']]['hotkey'])
+                available_coins -= rewards_to_build[0]['cost']
+                # Remove this reward from the list
+                rewards_to_build.pop(0)
+                # Increase the reward counter
+                rewards += 1
+                if offset > info.mods['reward']['max'] / (info.mods['reward']['percent'] / 100):
+                    offset = info.mods['reward']['max'] / (info.mods['reward']['percent'] / 100)
+        
+        
+        # If any cost == 1000000000, it means that we don't know the cost of this task
+        # So we need to find it
+        for cell in tasks:
+            if tasks[cell]['cost'] == 1000000000:
+                click(grid[cell])
+                tasks[cell]['cost'] = get_upgrade_cost()
+
+        # Get tasks keys sorted by cost
+        tasks_keys = list(tasks.keys())
+        tasks_keys.sort(key=lambda x: tasks[x]['cost'])
+
+
+
+        while len(tasks_keys) > 0:
+            # Get the first task
+            cell = tasks_keys[0]
+            task = tasks[tasks_keys[0]]
+
+            if available_coins <= task['cost']: break # If there are not enough coins to build or upgrade the next object, break the loop
+            if towers_order[0] != task['object'] and task['object_type'] == 'tower':
+                tasks_keys.pop(0)
+                continue   
+
+            available_coins -= task['cost']
+            offset = update_offset(rewards_to_build, rewards, offset, task['cost'])
+            click(grid[cell])
+            if task['object_type'] == 'tower':
+                _info = info.towers[task['object']]
+                this_object = towers
+                towers_order.append(towers_order.pop(0))
+            elif task['object_type'] == 'mod':
+                _info = info.mods[task['object']]
+                this_object = mods
+
+
+            if task['type'] == 'build':
+                press_key(_info['hotkey'])
+                if task['object_type'] == 'tower':
+                    towers[cell] = {
+                        'position': cell,
+                        'type': task['object'],
+                        'level': 0,
+                    }
+                    # If this is the first tower of this type, save it
+                    if task['object'] not in first_towers: first_towers[task['object']] = cell
+
+                elif task['object_type'] == 'mod':
+                    mods[cell] = {
+                        'position': cell,
+                        'type': task['object'],
+                        'level': 0,
+                    }
+
+            elif task['type'] == 'upgrade':
+                press_key('shift')
+
+            # If the object is max level, remove it from the tasks list
+            if this_object[cell]['level'] == _info['max_level']:
+                # If this is the last tower of this type in tasks, remove all this type from towers_order list
+                if task['object_type'] == 'tower' and [ x for x in tasks if tasks[x]['object'] == task['object'] ] == []:
+                    towers_order = [tower for tower in towers_order if tower != task['object']]
+                tasks.pop(cell)
+            else:
+                task['type'] = 'upgrade'
+                task['cost'] = get_upgrade_cost()
+                this_object[cell]['level'] += 1
+
+            tasks_keys = list(tasks.keys())
+            tasks_keys.sort(key=lambda x: tasks[x]['cost'])
+
+
+        print('Coins: '+str(coins))
+        print('Offset: '+str(offset))
 
         
-
-
-    # Print coin value
+        # Let's go on the list of towers and upgrade their num upgrades
+        for tower in first_towers:
+            click(grid[first_towers[tower]])
+            for upgrade in info.towers[tower]['num_upgrades']:
+                press_key(['ctrl', upgrade], 1, 0, 0.1)
 
 if __name__ == '__main__':
     # test
--- test.py
+++ test.py
@@ -6,6 +6,11 @@ import subprocess as sbp
 import csv
 
 # Get process info
-pinf = sbp.run('TASKLIST /FI "IMAGENAME eq infinitode-2.exe" /V /fo csv /nh', shell=True, stdout=sbp.PIPE, encoding='866').stdout
-pinf = list(csv.reader(pinf.splitlines(), delimiter=','))
-print(pinf)
+# pinf = sbp.run('TASKLIST /FI "IMAGENAME eq infinitode-2.exe" /V /fo csv /nh', shell=True, stdout=sbp.PIPE, encoding='866').stdout
+# pinf = list(csv.reader(pinf.splitlines(), delimiter=','))
+# print(pinf)
+
+# Get window info
+# winf = sbp.run('wmic path win32_desktopmonitor get screenheight, screenwidth', shell=True, stdout=sbp.PIPE, encoding='866').stdout
+# winf = list(csv.reader(winf.splitlines(), delimiter=' '))
+# print(winf)
